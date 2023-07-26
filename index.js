4512c5afb65c23dd50ab6ab249231ccc097642e5
Rinat <izmailov_rinat@list.ru>
2023-07-26 17:52:33 +0500

    dobavlenie elementov v massiv

--- index.js
+++ index.js
@@ -21,6 +21,38 @@ myArray1 === myArray2   // Будет false в результате
 
 // Если прировнять один массив к другому то при сравнении будет true
 
-const myArray3 = myArray
+const myArray3 = myArray   // Объект скопирован по ссылке
 
 myArray === myArray3
+
+                                // Чтение значений массива
+
+const myArray4 = [1, true, 'Rinat']
+    console.log(myArray4)
+
+// Если свойства у объекта цифровые (а в массиве так и есть, с 0 по длинне массива) то НЕЛЬЗЯ использовать точечную запись
+
+console.log(myArray4[0])
+console.log(myArray4[2])
+
+// Чтобы получить длинну массива используем "length"
+
+console.log(myArray4.length)
+
+// Можно менять значение элемента внутри массива:
+
+// Имеется массив
+
+const myArray5 = [1, 2, 3]
+    console.log(myArray5)
+    console.log(myArray5.length)
+
+// Указав после названия массива номер элемента и присвоить ему новое значение
+
+myArray5[2] = 'QWERTY' 
+    console.log(myArray5) // на выходе будет [1, 2, 'QWERTY]
+
+// Можем добавлять новый элемент в конец массива
+
+myArray5[3] = 'true' 
+    console.log(myArray5)
